4ecdbf828ad5cb4fdfab9993fc339a070c42d437
Jesse <jessebot.git@gmail.com>
2024-03-17 16:28:24 -0700

    add some comments

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	// TODO Steps
+	// - fetch from https://api.github.com/repos/github/gitignore/contents
+	// - find the correct type from the array by looking at <index>/name
+	// - also get download_url from at <index>
+	//
+	// e.g. GET https://api.github.com/repos/github/gitignore/contents[3].name = Ada.gitignore AND .download_url = raw.github.com/...
 }
